db34ff8d26b9247589ec8331f3fa941cad2ab077
htzy <ShiheHuang@foxmail.com>
2017-12-09 01:13:38 +0800

    更新清理环境脚本

--- clean_env.sh
+++ clean_env.sh
@@ -1,9 +1,11 @@
 # 删除idea配置
+echo "before clean: find . -name *.iml"
+echo | find . -name *.iml
 find . -name *.iml | xargs rm -f
 find . -name .idea | xargs rm -rf
+echo "after clean: find . -name *.iml"
+echo | find . -name *.iml
 # 删除编译后文件
 find . -name target | xargs rm -rf
 find . -name out | xargs rm -rf
 find . -name build | xargs rm -rf
-
-
